5147397eb8b50fc712b56bb3eaf89e44ac6aad1d
Tyler <tyler@burnamtech.com>
2018-07-23 12:40:28 -0700

    added basic file structure

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,38 +3,44 @@ const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
 
+// TODO: Does this work on windows?
+const mkdirp = require('mkdirp');
+
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
-    this.log(
-      yosay(
-        `Welcome to the funkadelic ${chalk.red('generator-dynamicscrm')} generator!`
-      )
-    );
+    this.log(yosay(`Welcome to the ${chalk.red('generator-dynamicscrm')} generator!\n`));
 
     const prompts = [
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
+        type: 'input',
+        name: 'solutionName',
+        message: 'What is the name of your solution?',
+        default: this.appname
       }
     ];
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
       this.props = props;
+      console.log(props);
     });
   }
 
   writing() {
+    // Build outer file structure
+    mkdirp.sync(this.props.solutionName);
+    mkdirp.sync(this.props.solutionName + '/build');
+    mkdirp.sync(this.props.solutionName + '/references');
+    mkdirp.sync(this.props.solutionName + '/scripts');
+
     this.fs.copy(
       this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.destinationPath(this.props.solutionName + '/dummyfile.txt')
     );
   }
 
   install() {
-    this.installDependencies();
+    // This.installDependencies();
   }
 };
